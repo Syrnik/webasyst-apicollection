68d1e0fc81d4ff5fa9d54659da48514be9e241b9
Serge Rodovnichenko <sergerod@gmail.com>
2026-03-13 02:52:25 +0300

    Add screenshots, png-icons and requirements.php

---
+++ lib/config/requirements.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'php'           => ['version' => '>=7.4.0', 'strict' => true],
+    'php.curl'      => ['description' => 'Доступ к API', 'strict' => true],
+    'app.installer' => ['strict' => true, 'version' => '>=3.0'],
+];
